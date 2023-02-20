f452b6b225237c520ad16d1d803f8b2f5db891ef
Gustavo Coelho Garcia <gcoelho.garcia1@gmail.com>
2023-02-20 17:55:14 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion ={};
 let acceptingAnswers = false;
@@ -52,6 +54,8 @@ getNewQuestion = () =>{
     }
     
     questionCounter++;
+    questionCounterText.innerText = questionCounter + "/" + MAXQ;//Atualiza dinamicamente o numero da questao xD
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -74,11 +78,22 @@ choices.forEach(choice =>{
 
         const classToApply = 
         selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        if(classToApply === 'correct' ){
+            incrementScore(BONUS);
+        }
+
         selectedChoice.parentElement.classList.add(classToApply);
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
         },1000);
-    })
-})
+    });
+});
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+};
+
 startGame();
