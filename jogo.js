5cf2eceeb48fd8c2e6c20fda2be5a2355a81b647
Gustavo Coelho Garcia <gcoelho.garcia1@gmail.com>
2023-03-29 21:31:20 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,100 +1,113 @@
-const question = document.getElementById("question");
+const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById("questionCounter");
-const scoreText = document.getElementById("score");
-
-let currentQuestion ={};
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions=[];
+let availableQuesions = [];
 
-let questions = [
-    {
-        question:"Quem é o atual presidente dos Estados Unidos?",
-        choice1: "Lula",
-        choice2: "Luffy",
-        choice3: "Biden",
-        choice4: "Manoel Gomes",
-        answer: 3
-    },
-    {
-        question:"Em que ano foi lançado o Playstation 4?",
-        choice1: "2013",
-        choice2: "1988",
-        choice3: "2014",
-        choice4: "2011",
-        answer: 1
-    },
-    {
-        question:"Quem é o atual maior pontuador de todos os tempos da NBA?",
-        choice1: "Michael Jordan",
-        choice2: "Lionel Messi",
-        choice3: "Rafael Lange",
-        choice4: "Lebron James",
-        answer: 4
-    }
-];
+let questions = [];
 
-//constantes
+fetch(
+    'https://opentdb.com/api.php?amount=20&category=31'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
 
-const BONUS = 10;
-const MAXQ = 3;
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions]
+    availableQuesions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
-getNewQuestion = () =>{
-    if(availableQuestions.length === 0 | questionCounter>= MAXQ ){
-        localStorage.setItem("pontuacaoMaisRecente", score)
-        return window.location.assign("/end.html");//quando terminar as perguntas, irá retornar ao menu :)
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        //go to the end page
+        return window.location.assign('/end.html');
     }
-    
     questionCounter++;
-    questionCounterText.innerText = questionCounter + "/" + MAXQ;//Atualiza dinamicamente o numero da questao xD
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
-    })
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
 
-    availableQuestions.splice(questionIndex, 1);
+    availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
 
-choices.forEach(choice =>{
-    choice.addEventListener("click", e =>{
-        if(!acceptingAnswers) return;
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        const selectedAnswer = selectedChoice.dataset['number'];
 
-        const classToApply = 
-        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-        if(classToApply === 'correct' ){
-            incrementScore(BONUS);
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
         }
 
         selectedChoice.parentElement.classList.add(classToApply);
+
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
-        },1000);
+        }, 1000);
     });
 });
 
-incrementScore = num => {
-    score +=num;
+incrementScore = (num) => {
+    score += num;
     scoreText.innerText = score;
 };
-
-startGame();
